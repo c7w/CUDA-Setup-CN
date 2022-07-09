fa8d73b8feb67999b2f947ecd3caff6eb3654ff6
c7w <admin@cc7w.cf>
2022-07-09 20:26:45 +0800

    Update proxy activation script

--- proxy/activate.sh
+++ proxy/activate.sh
@@ -1,7 +1,18 @@
 PORT=50000
-export HTTP_PROXY=http://localhost:$PORT
-export HTTPS_PROXY=http://localhost:$PORT
-export ALL_PROXY=http://localhost:$PORT
-export http_proxy=http://localhost:$PORT
-export https_proxy=http://localhost:$PORT
-export all_proxy=http://localhost:$PORT
+
+if [ -z $PROXY_ADDRESS ]
+then
+        export HTTP_PROXY=http://localhost:$PORT
+        export HTTPS_PROXY=http://localhost:$PORT
+        export ALL_PROXY=http://localhost:$PORT
+        export http_proxy=http://localhost:$PORT
+        export https_proxy=http://localhost:$PORT
+        export all_proxy=http://localhost:$PORT
+else
+        export HTTP_PROXY=http://$PROXY_ADDRESS:$PORT
+        export HTTPS_PROXY=http://$PROXY_ADDRESS:$PORT
+        export ALL_PROXY=http://$PROXY_ADDRESS:$PORT
+        export http_proxy=http://$PROXY_ADDRESS:$PORT
+        export https_proxy=http://$PROXY_ADDRESS:$PORT
+        export all_proxy=http://$PROXY_ADDRESS:$PORT
+fi
